549fe7e1106a94ac9955d041f39089257626fe9a
QuantumTraverse <BartschR0439@sduhsd.lan>
2017-05-05 14:34:18 -0700

    Tutorial #1: Some Progress
    learning JavaFX

---
+++ JavaFXTutorials/cdrRiker.java
@@ -0,0 +1,16 @@
+import.javafx.application.Application;
+import.javafx.event.ActionEvent;
+import.javafx.event.EventHandler;
+import.javafx.scene.Scene;
+import.javafx.scene.control.Button;
+import.javafx.scene.layout.StackPane;
+import.javafx.stage.Stage;
+//https://www.youtube.com/watch?v=FLkOX4Eez6o
+public class cdrRiker extends Application{
+	public static void main(String[]args) {
+		launch(args);
+	}
+	public void start(Stage primaryStage) throws Exception {
+		
+	}
+}
