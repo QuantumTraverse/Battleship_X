e126e9323681cbc4118328389bbfe887b3d3f29e
QuantumTraverse <BartschR0439@sduhsd.lan>
2017-05-09 14:35:23 -0700

    changes 9.May
    started using IntelliJ

---
+++ src/cdrRiker.java
@@ -0,0 +1,40 @@
+import javafx.application.*;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.*;
+
+public class cdrRiker extends Application {
+
+	Stage window;
+	Scene scene1, scene2;
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+		window = primaryStage;
+		Label label1 = new Label("Welcome to the first scene");
+		Button button1 = new Button("Go to scene 2");
+		button1.setOnAction(e -> window.setScene(scene2));
+
+		VBox layout1 = new VBox(20);
+		layout1.getChildren().addAll(label1, button1);
+		scene1 = new Scene(layout1, 200, 350);
+
+		Button button2 = new Button("This sucks, go back to scene 1");
+		button2.setOnAction(e -> window.setScene(scene1));
+
+		StackPane layout2 = new StackPane();
+		layout2.getChildren().add(button2);
+		scene2 = new Scene(layout2, 600, 300);
+
+		window.setScene(scene1);
+		window.setTitle("Title of the Window");
+		window.show();
+	}
+}
---
+++ src/ltData.java
@@ -0,0 +1,9 @@
+/**
+ * Created by bartschr0439 on 5/9/2017.
+ */
+public class ltData {
+    public static void display(String title, String message) {
+        Stage window = new Stage
+    }
+}
+//https://www.youtube.com/watch?v=SpL3EToqaXA
