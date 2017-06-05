d34e430140e4c2601a7704faf66e67ff921fadc2
QuantumTraverse <BartschR0439@sduhsd.lan>
2017-06-05 14:35:20 -0700

    changes 5.Jun

--- src/positioner.java
+++ src/positioner.java
@@ -1,12 +1,14 @@
 public class positioner {
     private int xcoord;
     private int ycoord;
+    private int age;
     public positioner(){
         this(0,0);
     }
     public positioner(int x, int y){
         xcoord = x;
         ycoord = y;
+        age = 0;
     }
     public void changeXPosition(int x){
         xcoord += x;
@@ -20,10 +22,16 @@ public class positioner {
     public int getYPosition(){
         return ycoord;
     }
+    public int getAge(){
+        return age;
+    }
     public boolean equals(positioner position) {
         if(position.getXPosition() == xcoord && position.getYPosition() == ycoord) {
             return true;
         }
         return false;
+    }
+    public void ager() {
+        age++;
     }
 }
