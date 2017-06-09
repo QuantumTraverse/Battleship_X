0152e1fe946db1f3e8d02cfabd0b1bf8a21555c4
QuantumTraverse <BartschR0439@sduhsd.lan>
2017-06-09 14:24:06 -0700

    changes 9.Jun.1

--- src/boardProduction.java
+++ src/boardProduction.java
@@ -2,12 +2,20 @@ import java.util.ArrayList;
 
 public class boardProduction {
     static String board;
-    public static String makeBoard(ArrayList<positioner> irradiatedArea) {
+    public static String makeBoard(ArrayList<positioner> irradiatedArea, positioner[] yourShots) { //enemy'sShips
         for(int row=10 ; row > 0 ; row-- ){
             for(int column=0 ; column < 10 ; column++ ){
+                boolean youNoFindAWatch = true;
+                for(positioner shot : yourShots) {
+                    if ((shot.getXPosition() == column) && (shot.getYPosition() == row) && youNoFindAWatch) {
+                        board += "X" + "\t";
+                        youNoFindAWatch = false;
+                    }
+                }
                 for(positioner coord : irradiatedArea) {
-                    if((coord.getXPosition() == column)&&(coord.getYPosition() == row))
+                    if((coord.getXPosition() == column)&&(coord.getYPosition() == row) && youNoFindAWatch)
                         board += "R"+"\t";
+                    youNoFindAWatch = false;
                 }
 
             }
@@ -15,17 +23,26 @@ public class boardProduction {
         }
         return board;
     }
-    public static String makeBoard(ship[] shipArray) {
+    public static String makeBoard(ship[] shipArray, positioner[] enemyShots) { //yourShips
         for(int row=10 ; row > 0 ; row-- ){
             for(int column=0 ; column < 10 ; column++ ){
-                for(ship boat : shipArray) {
-                    for(positioner coord : boat.getPosition()) {
-                        if ((coord.getXPosition() == column) && (coord.getYPosition() == row))
-                            board += "B" + "\t";
-                        else board += "~" + "\t";
+                boolean youNoFindAWatch = true;
+                for(positioner shot : enemyShots) {
+                    if ((shot.getXPosition() == column) && (shot.getYPosition() == row) && youNoFindAWatch) {
+                        board += "X" + "\t";
+                        youNoFindAWatch = false;
                     }
                 }
-
+                for(ship boat : shipArray) {
+                    for(positioner coord : boat.getPosition()) {
+                        if ((coord.getXPosition() == column) && (coord.getYPosition() == row) && youNoFindAWatch) {
+                            board += "B" + "\t";
+                            youNoFindAWatch = false;
+                        }
+                    }
+                }
+                if(youNoFindAWatch)
+                    board += "~" + "\t";
             }
             board += "\n";
         }
